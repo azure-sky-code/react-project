f8eb3ba6e703c48a11b3b06c4306b6607e12823c
sky <sdes7127@gmail.com>
2025-10-22 13:38:13 +0800

    [修改]icon + title

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,23 +7,23 @@ function Home() {
             <div className="home-text">
                 <Typewriter
                     onInit={(typewriter) => {
-                    typewriter
-                        .typeString('你好，我是 Wendy，是名「前端訓練家」！<br/><br/>')
-                        .pauseFor(800)
-                        
-                        .typeString('歡迎來到我的冒險回憶島，<br/>')
-                        .typeString('這裡收藏著我收服的寶可夢與旅程紀錄！<br/><br/>')
-                        .pauseFor(800)
-                        
-                        .typeString('我目前正在尋找新的冒險夥伴，<br/>')
-                        .typeString('如果你也正巧在找前端夥伴，<br/>')
-                        .typeString('且對我的旅程有興趣，歡迎與我聯絡！')
-                        .pauseFor(1000)
-                        .callFunction(() => {
-                            const cursor = document.querySelector('.Typewriter__cursor');
-                            if (cursor) cursor.style.display = 'none';
-                        })
-                        .start()
+                        typewriter
+                            .typeString('你好，我是 Wendy，是名「前端訓練家」！<br/><br/>')
+                            .pauseFor(800)
+                            
+                            .typeString('歡迎來到我的冒險回憶島，<br/>')
+                            .typeString('這裡收藏著我收服的寶可夢與旅程紀錄！<br/><br/>')
+                            .pauseFor(800)
+                            
+                            .typeString('我目前正在尋找新的冒險夥伴，<br/>')
+                            .typeString('如果你也正巧在找前端夥伴，<br/>')
+                            .typeString('且對我的旅程有興趣，歡迎與我聯絡！')
+                            .pauseFor(1000)
+                            .callFunction(() => {
+                                const cursor = document.querySelector('.Typewriter__cursor');
+                                if (cursor) cursor.style.display = 'none';
+                            })
+                            .start()
                     }}
                 />
             </div>
